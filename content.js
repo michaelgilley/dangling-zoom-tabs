e28e5602535483520a9ad256e65baedce9cbe6f9
Demian Ginther <st.diluted@gmail.com>
2020-06-17 10:22:36 -0600

    Update content.js
    Fixes for Zoom's re-design

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// Zoom site forwards the page to ?status=success after the meeting is launched.
-if (window.location.search.includes("status=success")) {
+// Zoom site forwards the page to #success after the meeting is launched.
+if (window.location.hash.includes("#success")) {
   chrome.runtime.sendMessage({ close: true });
 }
